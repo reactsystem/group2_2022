ef0c97d4df6dc4d9f4bb29fadabd148cac9af5cf
Nishimura <nishimurata.ppfpart.co.jp>
2022-05-02 12:04:45 +0900

    signup & login

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <h1 class="text-center mt-3 mb-3">勤怠管理システム</h1>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="h3 card-title text-center mt-2 mb-4">アカウント作成</h2>
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <!-- 名前 -->
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <!-- 部署名 -->
+                        <div class="row mb-3">
+                            <label for="department" class="col-md-4 col-form-label text-md-end">部署名</label>
+
+                            <div class="col-md-6">
+                                <!-- <input id="department" type="text" class="form-control" name="department" value="{{ old('department') }}" required autocomplete="department" autofocus> -->
+                                <select name="department" id="department" class="form-select" required autocomplete="department" autofocus>
+                                    <option hidden>選択してください</option>
+                                        <option value="">
+                                            部署名
+                                        </option>
+                                </select>
+                                @error('department')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <!-- メールアドレス -->
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <!-- パスワード -->
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <!-- 確認用パスワード -->
+                        <div class="row mb-3">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <!-- 登録ボタン -->
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
