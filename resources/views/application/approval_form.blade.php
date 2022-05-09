f4103a523f6b729d2d10cb12d711dd38eecb9aca
kyohei-ueda <ueda@ppfpart.co.jp>
2022-05-09 11:24:01 +0900

    申請一覧フォーム(モック)を作成しました

--- resources/views/application/approval_form.blade.php
+++ resources/views/application/approval_form.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('css')
+<link href="{{ asset('css/application-form.css') }}" rel="stylesheet">
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
--- resources/views/personal_management.blade.php
+++ resources/views/personal_management.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('css')
+<link href="{{ asset('css/input.css') }}" rel="stylesheet">
+@endsection
+
     <header>
     </header>
     @section('content')
