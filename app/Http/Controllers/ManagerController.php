a9d0252e553a4a84a61d4209dffb1f97e383bd6f
tsujimura <tsujimura@ppfpart.co.jp>
2022-05-09 15:17:37 +0900

    不要行削除

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -4,11 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
-use App\Models\ApplicationType;
-use App\Models\Department;
-use App\Models\FixedTime;
-use App\Models\WorkTime;
-use App\Models\WorkType;
 
 class ManagerController extends Controller
 {
