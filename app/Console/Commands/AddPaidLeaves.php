04c956ddae992ac45204c78f381a7f6d19eaac48
kyohei-ueda <ueda@ppfpart.co.jp>
2022-05-13 17:58:17 +0900

    Commandを作成

--- app/Console/Commands/AddPaidLeaves.php
+++ app/Console/Commands/AddPaidLeaves.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\User;
+use App\Models\PaidLeave;
+use App\Models\AddPaidLeave;
 
 class AddPaidLeaves extends Command
 {
@@ -28,6 +30,44 @@ class AddPaidLeaves extends Command
      */
     public function handle()
     {
-        $users = User::where('joining', '=', '');
+        $users = User::all();
+
+        foreach ($users as $user) {
+            // 勤続年数を計算
+            $user_years = (strtotime("now") - strtotime($user->joining)) / 86400 / 365 ;
+
+            if ($user_years == 0.5) {
+                $add_paid_leave = AddPaidLeave::find(1);
+                $user->paidLeave->left_days += $add_paid_leave->add_days;
+            }
+            if ($user_years == 1.5) {
+                $add_paid_leave = AddPaidLeave::find(2);
+                $user->paidLeave->left_days += $add_paid_leave->add_days;
+            }
+            if ($user_years == 2.5) {
+                $add_paid_leave = AddPaidLeave::find(3);
+                $user->paidLeave->left_days += $add_paid_leave->add_days;
+            }
+            if ($user_years == 3.5) {
+                $add_paid_leave = AddPaidLeave::find(4);
+                $user->paidLeave->left_days += $add_paid_leave->add_days;
+            }
+            if ($user_years == 4.5) {
+                $add_paid_leave = AddPaidLeave::find(5);
+                $user->paidLeave->left_days += $add_paid_leave->add_days;
+            }
+            if ($user_years == 5.5) {
+                $add_paid_leave = AddPaidLeave::find(6);
+                $user->paidLeave->left_days += $add_paid_leave->add_days;
+            }
+            if ($user_years == 6.5) {
+                $add_paid_leave = AddPaidLeave::find(7);
+                $user->paidLeave->left_days += $add_paid_leave->add_days;
+            }
+
+            // TODO:7.5年以降の処理を追加,有給付与上限の場合は超えた分を追加しない設定を追加
+
+        }
+
     }
 }
