52881153456954795b4528bd2c8968adc3537ad8
tsujimura <tsujimura@ppfpart.co.jp>
2022-05-13 12:02:43 +0900

    部署勤怠管理フォーム作成

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,13 @@ use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 // 勤怠入力フォーム
-Route::get('input', function () {
+Route::get('/', function () {
     return view('input');
-})->name('input');
+})->name('input')->middleware('auth');
 
 //社員管理フォーム
-Route::group(['prefix' => 'employees', 'as' => 'employees.',], function(){
+Route::group(['prefix' => 'employees', 'as' => 'employees.', 'middleware' => 'auth'], function(){
     Route::get('/', [EmployeesFormController::class, 'show'])->name('show');
     Route::get('/create', [EmployeesFormController::class, 'create'])->name('create');
     Route::get('/edit', [EmployeesFormController::class, 'edit'])->name('edit');
@@ -41,17 +37,21 @@ Route::get('/personal_management', function () {
     return view('personal_management');
 })->name('mgmt.personal');
 
-//各種申請フォーム
-Route::get('/application-form', [ApplicationFormController::class, 'index'])->name('app.index');
+//申請フォーム
+Route::group(['prefix' => 'application', 'as' => 'application.', 'middleware' => 'auth'], function(){
+    Route::get('/', [ApplicationFormController::class, 'index'])->name('index');
+    Route::get('/form', [ApplicationFormController::class, 'show'])->name('show');
+    Route::post('/form/{user}', [ApplicationFormController::class, 'create'])->name('create');
+    Route::get('/approval/{user}/{application}', [ApplicationFormController::class, 'approve'])->name('approve');
+});
+
 
 //申請承認フォーム
-Route::get('/approval-form', [ApplicationFormController::class, 'approve'])->name('app.approval');
-
-//申請一覧フォーム
-Route::get('/show-form', [ApplicationFormController::class, 'show'])->name('app.show');
+// Route::get('/approval-form', [ApplicationFormController::class, 'approve'])->name('app.approval');
 
 // 部署勤怠管理フォーム
 Route::get('/management', [ManagerController::class, 'index'])->name('mgmt.dept');
+Route::post('/management', [ManagerController::class, 'index'])->name('mgmt.dept.post');
 
 // マスタ管理フォーム
-Route::get('/management/master', [ManagerController::class, 'getMaster'])->name('mgmt.master');
+Route::get('/management/master', [ManagerController::class, 'getMaster'])->name('master');
