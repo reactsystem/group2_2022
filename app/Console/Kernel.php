eb133741d064e0c648f7902d1617dea0d2c6f62d
Nishimura <nishimurata.ppfpart.co.jp>
2022-05-26 12:45:52 +0900

    kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,11 +27,18 @@ class Kernel extends ConsoleKernel
             $diff_months = $joining_date->diffInMonths($now);
             
             //月の入社日の日で更新(新入社員6ヶ月後、以降1年ごと)
-            if($diff_months % 12 === 6){
-                $schedule->command('addPaidLeave')->monthlyOn($joining_day, '00:00');
-            }
+            // if($diff_months % 12 === 6){
+            //     $schedule->command('addPaidLeave')->monthlyOn($joining_day, '00:00');
+            // }
+            
+            // テスト用
+            $schedule->command('addPaidLeave')->everyMinute();
+
             //1日に一回有給休暇取得実行
-            $schedule->command('getPaidLeave')->daily();
+            // $schedule->command('getPaidLeave')->daily();
+
+            // テスト用
+            $schedule->command('getPaidLeave')->everyMinute();
         }
     }
 
