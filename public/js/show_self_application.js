f0511d8bb8f8450ec15f77e273da9a05571924be
kyohei-ueda <ueda@ppfpart.co.jp>
2022-05-30 09:31:04 +0900

    仮コミット

--- public/js/show_self_application.js
+++ public/js/show_self_application.js
@@ -1,5 +1,7 @@
 const searchForm = document.getElementById('search')
 const limit = document.getElementById('limit');
+const appTypeSelect = document.getElementById('app-type-edit');
+
 limit.addEventListener('change', (e)=>{
     searchForm.submit();
 })
@@ -69,6 +71,71 @@ $(document).ready(function() {
 		modal.find('.modal-body input#app-date-edit').val(data_date);
 		modal.find('.modal-body input#app-start-edit').val(data_start);
 		modal.find('.modal-body input#app-end-edit').val(data_end);
+
+		//初期設定(DateTimePickerをTime型に)
+		iniTime();
+
+		if (appTypeSelect.value == 4) {
+			overTimeRequire();
+
+			const left_time = $('#left_time').data();
+
+			//開始時間、終了時間(終業時間より後のみ選択可)
+            $('#startTimePicker').datetimepicker('minDate', moment({h:left_time.name.slice(0, 2), m:left_time.name.slice(3, 5)}));  
+            $('#endTimePicker').datetimepicker('minDate', moment({h:left_time.name.slice(0, 2), m:left_time.name.slice(3, 5)}));
+
+            //開始時間、終了時間のバリデーション
+            let limitStartTime = left_time.name
+
+            $('[name=start_time]').on('blur', function(e){
+                // 始業時間と終業時間のエラーメッセージが空なら申請ボタンを押せないように
+                if($('.endTimeErrorMsg').text('') && $('.startTimeErrorMsg').text('') || $('.endTimeErrorMsg').is(':empty') && $('.startTimeErrorMsg').is(':empty')){
+                    $('#btn-update').prop("disabled", false); 
+                }
+                
+                //開始時間が終業時間より早かったらエラーメッセージ
+                if(e.target.value.replace(':', '') < left_time.name.replace(':', '')){
+                    $('.startTimeError').removeClass('d-none');
+                    $('.startTimeErrorMsg').text(`${limitStartTime}分以降を選択してください。`);
+                    $('#btn-update').prop("disabled", true); 
+                }else{
+                    $('.startTimeErrorMsg').text('');
+                    $('.startTimeError').addClass('d-none');
+                }
+                
+                $('[name=end_time]').on('blur', function(val){
+                    const endInputTime = val.target.value.replace(':', '');
+                    
+                    //終了時間が終業時間より早かったらエラーメッセージ
+                    if(endInputTime <= left_time.name.replace(':', '')){
+                        $('.endTimeError').removeClass('d-none');
+                        $('.endTimeErrorMsg').text(`${limitStartTime}分以降を選択してください。`);
+                        $('#btn-update').prop("disabled", true);
+                    
+                    //終了時間が開始時間より早かったらエラーメッセージ
+                    }else if(endInputTime <= e.target.value.replace(':', '')){
+                        $('.endTimeError').removeClass('d-none');
+                        $('.endTimeErrorMsg').text(`開始時間よりも遅い時間を選択してください。`); 
+                        $('#btn-update').prop("disabled", true); 
+                    }else{
+                        $('.endTimeErrorMsg').text('');
+                        $('.endTimeError').addClass('d-none');
+                    }
+                    //開始時間が終業時間より早かったらエラーメッセージ
+                    if(e.target.value.replace(':', '') < left_time.name.replace(':', '')){
+                        $('.startTimeError').removeClass('d-none');
+                        $('.startTimeErrorMsg').text(`${limitStartTime}分以降を選択してください。`);
+                        $('#btn-update').prop("disabled", true);
+                    }
+                })
+            })
+
+		} else if (appTypeSelect.value == 5) {
+			editTimeRequire();
+		} else {
+			timeDisable();
+		}
+
 	});
 
 		// 「申請再利用フォーム」に値を渡す
@@ -149,15 +216,20 @@ $(document).ready(function() {
 			$('.endTimeError').addClass('d-none');
 		}	
 
-	const appTypeSelect = document.getElementById('app-type-edit');
-
 	appTypeSelect.addEventListener('change', (e)=>{
 
 		//初期設定(DateTimePickerをTime型に)
         iniTime();
 
+		$('#startTimePicker').datetimepicker('minDate', null);
+		$('#endTimePicker').datetimepicker('minDate', null);
+
         // HTMLで直接設定したエラーメッセージを削除
         DelErrMsg();
+
+		// 開始時間、終了時間を初期化
+		$('[name=start_time]').val('');
+		$('[name=end_time]').val('');		
 
 		if (appTypeSelect.value == 4) {
 			overTimeRequire();
@@ -216,6 +288,8 @@ $(document).ready(function() {
 
 		} else if (appTypeSelect.value == 5) {
 			editTimeRequire();
+			iniTime();
+
 		} else {
 			timeDisable();
 		}
