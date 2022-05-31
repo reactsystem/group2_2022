46fd841821782a991e9f642e96b318fbb3c2782c
Nishimura <nishimurata.ppfpart.co.jp>
2022-05-31 14:12:32 +0900

    batch

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Models\AddPaidLeave;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Models\User;
@@ -24,18 +25,30 @@ class Kernel extends ConsoleKernel
         
         // 本番用
         $now = Carbon::now();
-        $users = User::all();
+        $users = User::whereNull('leaving')->get();
         foreach($users as $user){
             $joining = $user->joining;
             $joining_date = new Carbon($joining);
             $joining_day = $joining_date->day;
             $diff_months = $joining_date->diffInMonths($now);
+
+            // 有給付与年数最小値
+            $min_year = AddPaidLeave::min('years');
+
+            // 2つ目の有給付与年数取得
+            $sec_year = AddPaidLeave::find(2)->years;
             
             
             // 月の入社日の日で更新(新入社員6ヶ月後、以降1年ごと)
-            if($diff_months % 12 === 6){
-                // $schedule->command('command:addPaidLeave')->monthlyOn($joining_day, '00:00');
-            }
+            // if($min_year != 0){
+            //     if($diff_months % 12*(int)$min_year == 0){
+            //         $schedule->command('command:addPaidLeave')->monthlyOn($joining_day, '00:00');
+            //     }
+            // }elseif($min_year == 0){
+            //     if($diff_months % 12*(int)$sec_year == 0){
+            //         $schedule->command('command:addPaidLeave')->monthlyOn($joining_day, '00:00');
+            //     }
+            // }
             
             // 1日に一回有給休暇取得実行
             // $schedule->command('command:getPaidLeave')->dailyAt(FixedTime::first()->start_time);           
